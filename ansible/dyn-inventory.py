465ac7aa5fa4706249925e82c82e58034523db4d
Вячеслав Тихонов <vtihonov@astralinux.ru>
2021-07-13 22:54:10 +0300

    hw-08: ansible-1 main code

---
+++ ansible/dyn-inventory.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import json
+from os import write
+
+# fetching data from terraform state
+f = open('../terraform/stage/terraform.tfstate')
+data = json.load(f)
+f.close
+# parse json content
+appserver_ip = data['outputs']['external_ip_address_app']['value']
+dbserver_ip = data['outputs']['external_ip_address_db']['value']
+
+# print out dynamic inventory
+json_out= json.dumps({'app':{'hosts': [appserver_ip]}, 'db':{'hosts': [dbserver_ip]}}, sort_keys=True, indent = 4)
+print(json_out)
+f = open('inventory.json', 'w')
+f.write(json_out.replace('[','{').replace(']','}'))
+f.flush
+f.close
